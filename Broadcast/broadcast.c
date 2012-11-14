24c97cb5b4b7377a45472a106186b477ca8bc95b
Maarten de Jonge <mdejonge1990@gmail.com>
2012-11-14 12:08:03 +0100

    some cleaning up of the broadcast function

--- Broadcast/broadcast.c
+++ Broadcast/broadcast.c
@@ -1,5 +1,6 @@
 #include "broadcast.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int MYMPI_Bcast(void *buffer, int count,
                 MPI_Datatype datatype, int root,
@@ -13,30 +14,32 @@ int MYMPI_Bcast(void *buffer, int count,
     // the meeting point is the node (roughly) opposite the root, where the flow of data
     // will meet itself (normalised such that root = 4)
     int meeting_point = size / 2;
-    //printf("Root: %d, meeting point: %d\n", root, root + meeting_point);
 
     // the root process sends the data to its 2 neighbours
     if (pnum == root) {
         // make 2 requests, the p and m stand for plus and minnus
-        MPI_Request request_p,
-                    request_m;
-        MPI_Request request_arr[2] = {request_p, request_m}; // used for MPI_Waitall
+        MPI_Request request_arr[2];
+        MPI_Status status_arr[2];
 
-        printf("Sending: %d\n", ((int *)buffer)[0]);
-        MPI_Isend(buffer, count, datatype, root + 1, tag, communicator, &request_p);
-        MPI_Isend(buffer, count, datatype, root - 1, tag, communicator, &request_m);
+        MPI_Isend(buffer, count, datatype, root + 1, tag, communicator, &request_arr[0]);
+        MPI_Isend(buffer, count, datatype, root - 1, tag, communicator, &request_arr[1]);
 
         // return 0 if both messages arrived, else 1
-        return 0;
+        if (MPI_Waitall(2, request_arr, status_arr) == MPI_SUCCESS) {
+            return 0;
+        } else {
+            printf("Error: Message(s) not received.\n");
+            return 1;
+        }
     }
 
-    // if the process is the meeting point, receive from either side
+    // if the process is the meeting point, receive from the lower ranked
+    // process just because
     else if (pnum == root + meeting_point) {
         MPI_Status status_p,
                    status_m;
 
         MPI_Recv(buffer, count, datatype, (pnum - 1) % size, tag, communicator, &status_m);
-        printf("Process %d received: %d\n", pnum, ((int*)buffer)[0]);
     }
 
     // if the process rank is larger than the root, receive from the lower
@@ -46,7 +49,6 @@ int MYMPI_Bcast(void *buffer, int count,
                    status_s;
 
         MPI_Recv(buffer, count, datatype, (pnum - 1) % size, tag, communicator, &status_r);
-        printf("Process %d received: %d\n", pnum, ((int *)buffer)[0]);
 
         if (MPI_Send(buffer, count, datatype, (pnum + 1) % size, tag, communicator) == MPI_SUCCESS) {
         } else {
@@ -62,7 +64,6 @@ int MYMPI_Bcast(void *buffer, int count,
                    status_s;
 
         MPI_Recv(buffer, count, datatype, (pnum - 1) % size, tag, communicator, &status_r);
-        printf("Process %d received: %d\n", pnum, ((int *)buffer)[0]);
 
         if (MPI_Send(buffer, count, datatype, (pnum + 1) % size, tag, communicator) == MPI_SUCCESS) {
             return 0;
@@ -78,15 +79,27 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     int pnum, size;
     int tag = 0;
+    int root = 2;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &pnum);
 
     int message[1];
-    if (pnum = 2) {
-        message[0] = 8;
+
+    if (pnum == root) {
+        message[0] = 42;
+    } else {
+        printf("Proof that message is unitialised: %d\n", message[0]);
     }
 
-    MYMPI_Bcast(&message, 1, MPI_INT, 2, MPI_COMM_WORLD);
+    if (MYMPI_Bcast(&message, 1, MPI_INT, 2, MPI_COMM_WORLD)) {
+        printf("Error: Broadcast failed.\n");
+    }
+
+    if (pnum == root) {
+        printf("Process %d sent: %d\n", pnum, message[0]);
+    } else {
+        printf("Process %d received: %d\n", pnum, message[0]);
+    }
 
     MPI_Finalize();
 
