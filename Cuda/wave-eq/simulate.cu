015f82b019299ef99cad0422164de529ea95c658
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-04 14:15:17 +0100

    implement wave-equation cuda code

--- Cuda/wave-eq/simulate.cu
+++ Cuda/wave-eq/simulate.cu
@@ -33,8 +33,18 @@ static void checkCudaCall(cudaError_t result) {
 }
 
 // The kernel that simulates an iteration of the wave equation on the GPU
-__global__ void simulationKernel(double *old, double *current, double *next) {
+__global__ void simulationKernel(int stepsize, int max,
+                                 double *old,
+                                 double *current,
+                                 double *next)
+{
     // determine the boundaries for this particular block
+    int i_min = 1 + blockIdx.x * stepsize;
+    int i_max = i_min + stepsize;
+
+    /* make sure that i_max doesn't get too large
+     * TODO: handle the intervals more neatly. */
+    i_max = (i_max > max ? max : i_max);
 
     // the main simulation loop
     for (int i = i_min; i < i_max; ++i) {
@@ -61,15 +71,53 @@ __global__ void simulationKernel(double *old, double *current, double *next) {
 double *simulate(const int i_max, const int t_max, const int num_threads,
                  double *old_array, double *current_array, double *next_array)
 {
+    int stepsize = ((i_max - 2) / num_threads) + 1;
+
+    // allocate the vectors on the GPU
+    float* d_old = NULL;
+    checkCudaCall(cudaMalloc((void **) &d_old, i_max * sizeof(double)));
+    if (d_old == NULL) {
+        cout << "could not allocate memory!" << endl;
+        return;
+    }
+
+    double* d_current = NULL;
+    checkCudaCall(cudaMalloc((void **) &d_current, i_max * sizeof(double)));
+    if (d_current == NULL) {
+        checkCudaCall(cudaFree(deviceA));
+        cout << "could not allocate memory!" << endl;
+        return;
+    }
+
+    double* d_next = NULL;
+    checkCudaCall(cudaMalloc((void **) &d_next, i_max * sizeof(double)));
+    if (d_next == NULL) {
+        checkCudaCall(cudaFree(deviceA));
+        checkCudaCall(cudaFree(deviceB));
+        cout << "could not allocate memory!" << endl;
+        return;
+    }
+
+    // copy the data to the GPU
+    checkCudaCall(cudaMemcpy(d_old, old_array, i_max*sizeof(double), cudaMemcpyHostToDevice));
+    checkCudaCall(cudaMemcpy(d_current, current_array, i_max*sizeof(double), cudaMemcpyHostToDevice));
+    checkCudaCall(cudaMemcpy(d_next, next_array, i_max*sizeof(double), cudaMemcpyHostToDevice));
+
+    // the main loop
     for (int t = 0; t < t_max; ++t) {
-        add <<< num_threads, 1 >>> (old_dev, curr_dev, next_dev);
+        add <<< num_threads, 1 >>> (stepsize, max, old_dev, curr_dev, next_dev);
 
         // swap the arrays around
-        double *temp_old = old_array;
-        old_array = current_array;
-        current_array = next_array;
-        next_array = temp_old;
+        double *temp_old = d_old;
+        d_old = d_current;
+        d_current = d_next;
+        d_next = temp_old;
     }
+
+    // copy the data back to the main program
+    checkCudaCall(cudaMemcpy(old_array, d_old, i_max*sizeof(double), cudaMemcpyDeviceToHost));
+    checkCudaCall(cudaMemcpy(current_array, d_current, i_max*sizeof(double), cudaMemcpyDeviceToHost));
+    checkCudaCall(cudaMemcpy(next_array, d_next, i_max*sizeof(double), cudaMemcpyDeviceToHost));
 
     return current_array;
 }
