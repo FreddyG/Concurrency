09e84cf5b5a08c43e9990c287c14dbd861a1390f
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-11 16:19:03 +0100

    add experiments bash script to wave-eq

---
+++ Cuda/wave-eq/experiments.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# check if running locally or on DAS-4
+if [ $1 = local ]
+then
+    COMMAND="./assign3_1"
+else
+    COMMAND="prun -v -np 1 -native '-l gpu=GTX480' wave"
+fi
+
+N=4
+I_VALS=(1000 10000 1000000 10000000)
+T_VALS=(1000000 500000 5000 500)
+
+# clear the output file
+echo > exp_results.txt
+
+for ((i=0; i < 4; i++))
+do
+    # loop through the number of threads
+    for threads in 1 2 4 8 16; do
+        export OMP_NUM_THREADS=$threads
+        echo Doing ${I_VALS[$i]} ${T_VALS[$i]}
+
+        echo Parameters: ${I_VALS[i]} ${T_VALS[$i]} >> exp_results.txt
+        $COMMAND ${I_VALS[$i]} ${T_VALS[$i]} >> exp_results.txt
+        echo -e "\n\n" >> exp_results.txt
+    done
+done
