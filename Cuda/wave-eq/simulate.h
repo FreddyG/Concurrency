1bc86d2e88efa5dd2b542d56e129f5cc4a78ec65
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-11 16:08:01 +0100

    fix simulate header declaration

--- Cuda/wave-eq/simulate.h
+++ Cuda/wave-eq/simulate.h
@@ -4,5 +4,5 @@
 
 #pragma once
 
-double *simulate(const int i_max, const int t_max, const int num_cpus,
+double *simulate(const int i_max, const int t_max,
         double *old_array, double *current_array, double *next_array);
