627ffe11a3b4e99af65466e225576b6a09b9f871
CPP course <cpp1216@fs0.cm.cluster>
2012-12-12 12:20:36 +0100

    update wave experiment script

--- Cuda/wave-eq/experiments.sh
+++ Cuda/wave-eq/experiments.sh
@@ -10,11 +10,12 @@ echo > exp_results.txt
 
 for ((i=0; i < 4; i++))
 do
-# loop through the number of threads
-    export OMP_NUM_THREADS=$threads
+    for ((blah=0; blah < 10; blah++))
+    do
     echo Doing ${I_VALS[$i]} ${T_VALS[$i]}
 
     echo Parameters: ${I_VALS[i]} ${T_VALS[$i]} >> exp_results.txt
     prun -v -np 1 -native '-l gpu=GTX480' wave ${I_VALS[$i]} ${T_VALS[$i]} >> exp_results.txt
-    echo -e "\n\n" >> exp_results.txt
+    echo -e "\n" >> exp_results.txt
+    done
 done
